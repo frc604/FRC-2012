c1dcb5da985691aa187f0c3d2062b0a93d5f806c
Michael Smith <mdsmtp@gmail.com>
2012-02-04 17:38:30 -0800

    Added link to Vision.h

--- Vision.h
+++ Vision.h
@@ -4,4 +4,6 @@
  * See branch "vision-testing", where testing and construction
  * of the vision code are going on on the 2009 robot.
  * 
+ * https://github.com/frc604/FRC-2012/tree/vision-testing
+ * 
  */
