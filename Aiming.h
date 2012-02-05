cff20de329b41b0422c03e88a0a7107911e13192
Michael Smith <mdsmtp@gmail.com>
2012-02-04 17:32:47 -0800

    Placeholder Aiming.h file, why not?

---
+++ Aiming.h
@@ -0,0 +1,10 @@
+/* 
+ * TODO: Actually put something here.
+ * 
+ * What should go here?
+ * ====================
+ *  - Distance calculation code.
+ *  - Angle calculation code.
+ *  - Aiming code.
+ * 
+ */
