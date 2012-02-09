b1287de7cb3f437e1e0cef69cd1955e2345fd154
kevin <kevin.m.parker@gmail.com>
2012-02-09 15:51:41 -0800

    Added a few versions of spatial tracking

---
+++ Aiming.h
@@ -0,0 +1,40 @@
+/* 
+ * TODO: Actually put something here.
+ * 
+ * What should go here?
+ * ====================
+ *  - Distance calculation code.
+ *  - Angle calculation code.
+ *  - Aiming code.
+ * 
+ */
+
+
+
+
+
+
+
+
+
+//===== Kevin's simple versions: =====
+
+#define kx 0
+#define ky 0
+#define heightOftarget 18 //inches
+#define widthOftarget 24 //inches
+
+void getSpacialRelationshipToTarget(float* dist, float* angle, float* angleOfTarget, float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
+	//TODO - fill in stuff
+}
+
+void getRelXYZOfTarget(float* x, float* y, float* z, float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
+	z = kx * 2 * heightOfTarget / (y1 + y2 - y3 - y4);
+	//xs = kx * x / z
+	//x = xs * z / kx
+	float avgx = (x1 + x2 + x3 + x4)/4;
+	float avgy = (y1 + y2 + y3 + y4)/4;
+
+	x = avgx * z / kx;
+	y = avgy * z / ky;
+}
