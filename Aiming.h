fa57f9a539fcae40c05342fd2289376d75256af0
kevin <kevin.m.parker@gmail.com>
2012-02-09 17:51:56 -0800

    Bug fixes, changed kx and ky calculation

--- Aiming.h
+++ Aiming.h
@@ -1,3 +1,5 @@
+#include <math.h>
+
 /* 
  * TODO: Actually put something here.
  * 
@@ -19,22 +21,27 @@
 
 //===== Kevin's simple versions: =====
 
-#define kx 1 // TODO - calibrate
-#define ky 1 // TODO - calibrate
+const float FOV = 47;//degrees
+const float pixelsWide = 640;
+const float kx = tan(FOV*PI / 180) / pixelsWide * 2;
+const float ky = kx;
 #define heightOftarget 18 //inches
 #define widthOftarget 24 //inches
+
 
 void getSpacialRelationshipToTarget(float* dist, float* angle, float* angleOfTarget, float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
 	//TODO - fill in stuff
 }
 
 void getRelXYZOfTarget(float* x, float* y, float* z, float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
-	z = kx * 2 * heightOfTarget / (y1 + y2 - y3 - y4);
-	//xs = kx * x / z
-	//x = xs * z / kx
+	(*z) = kx * 2 * heightOftarget / (y1 + y2 - y3 - y4);
+		//xs = kx * x / z
+		//x = xs * z / kx
 	float avgx = (x1 + x2 + x3 + x4)/4;
 	float avgy = (y1 + y2 + y3 + y4)/4;
 
-	x = avgx * z / kx;
-	y = avgy * z / ky;
+	(*x) = avgx * (*z) / kx;
+	(*y) = avgy * (*z) / ky;
 }
+
+
