e4751d4a916d7491de1773e4b7b6c03fff9f6c09
Michael Smith <mdsmtp@gmail.com>
2012-02-11 22:38:45 -0800

    Completely rewrote the camera code; increased modularization; rudimentary angle calculation stuffs put in for testing and calibration.

--- VisionTesting.cpp
+++ VisionTesting.cpp
@@ -1,8 +1,16 @@
 #include <nivision.h>
 //#include <niimaq.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include "WPILib.h"
-#include "ImageProcessing.cpp"
-#include "CameraData.cpp"
+#include "CameraControl.h"
+#include "PotentiometerMonitor.cpp"
+#include "TurretOutput.cpp"
+
+using namespace std;
 
 /* Port configuration for sensors and actuators. */
 	#define MANIPULATOR_JOYSTICK_PORT 1
@@ -16,149 +24,194 @@
 	#define FEED_MOTOR_PORT 7
 
 	#define POTENTIOMETER_PORT 7
+	#define GYRO_PORT 1
+
+	#define LIGHT_RELAY_PORT 1
 
 /* Button configuration. */
 	
 
-/* Actuator polarity and speed configuration. */
-	#define TURRET_POWER 0.4
-
 /* Sensor configuration. */
-	#define POTENTIOMETER_LEFT_VOLTAGE 1.7
-	#define POTENTIOMETER_RIGHT_VOLTAGE 0.1
+	#define TURRET_POWER -0.4
 
 class VisionTesting : public SimpleRobot {
 	Joystick joystickManipulator;
 	Victor motorTurret;
+	
 	AnalogChannel analogPotentiometer;
+	Gyro gyro;
 	
 	Task cameraTask;
+	
+	PotentiometerMonitor *potentiometerMonitor;
+	TurretOutput *turretOutput;
+	PIDController *turretController;
+	
+	Relay relayLight;
 	
 	public:
 		VisionTesting (void):
 			joystickManipulator(MANIPULATOR_JOYSTICK_PORT),
 			motorTurret(TURRET_MOTOR_PORT),
 			analogPotentiometer(POTENTIOMETER_PORT),
-			cameraTask("cameraTask", (FUNCPTR)(CameraProcess))
+			gyro(GYRO_PORT),
+			cameraTask("cameraTask", (FUNCPTR)(CameraProcess)),
+			relayLight(LIGHT_RELAY_PORT, Relay::kForwardOnly)
 		{
 			GetWatchdog().SetEnabled(false);
+			
+			potentiometerMonitor = new PotentiometerMonitor(&analogPotentiometer);
+			turretOutput = new TurretOutput(&motorTurret);
+			turretController = new PIDController(0.009, 0, 0.0011, potentiometerMonitor, turretOutput);
+			turretController->SetSetpoint(90);
 		}
-		
-		static int CameraProcess() {
-			// TODO: Test.
-			// TODO: Add error handling.
-			
-			Image *image;
-			IVA_Data *imageData;
-			IVA_Result *particleResults;
-			int targets;
-			
-			double currentX = 0.0;
-			double currentY = 0.0;
-			
-			double centerX = 0.0;
-			double centerY = 0.0;
-			
-			Timer *second = new Timer();
-			int fps = 0;
-			
-			int totalFrames = 0;
-			
-			second->Start();
-			
-			while(CameraData::GetInstance().enabled) {
-				if(AxisCamera::GetInstance().IsFreshImage()) {
-					AxisCamera::GetInstance().GetImage(image);
-					imageData = IVA_ProcessImage(image);
-					particleResults = imageData->stepResults[9].results;
-					
-					targets = (int) particleResults->resultVal.numVal;
-					particleResults++;
-					
-					for(int i = 0; i < targets; i++) {
-						currentX = particleResults->resultVal.numVal;
-						particleResults++;
-						currentY = particleResults->resultVal.numVal;
-						particleResults++;
-						
-						if(currentY > centerY) {
-							centerX = currentX;
-							centerY = currentY;
-						}
-					}
-					
-					IVA_DisposeData(imageData);
-					
-					totalFrames++;
-					
-					CameraData::GetInstance().centerX = centerX;
-					CameraData::GetInstance().centerY = centerY;
-					
-					CameraData::GetInstance().frames = totalFrames;
-					
-					if(second->Get() >= 1) {
-						CameraData::GetInstance().fps = fps;
-						
-						fps = 0;
-						second->Reset();
-					}
-					
-					fps++;
-				}
-			}
-			
-			second->Stop();
-			
-			return 0;
-		}
-
-		float PotentiometerToDegrees(float voltage) {
-			return (voltage - POTENTIOMETER_RIGHT_VOLTAGE) / (POTENTIOMETER_LEFT_VOLTAGE - POTENTIOMETER_RIGHT_VOLTAGE) * 180; 
-		}
-		
-		float CameraXToDegrees(float cameraX) {
-			return 90; // TODO: Implement this.
-		}
-		
-		float CameraYToDegrees(float cameraY) {
-			return 90; // TODO: Implement this.
-		}
-		
+				
 		void Autonomous (void) {
 			GetWatchdog().SetEnabled(false);
 		}
 
 		void OperatorControl (void) {
 			GetWatchdog().SetEnabled(true);
+			GetWatchdog().Feed();
 			
-			float turretPower;
-
+			double gyroPosition;
+			double currentPosition;
+			double lastPosition;
+			
+			CameraData &cameraData = CameraData::GetInstance();
+			
+			Timer *runTimer = new Timer();
+			runTimer->Start();
+			
+			Timer *gyroTimer = new Timer();
+			gyroTimer->Start();
+			
+			float turretPower = 0;
+			
+			bool wasEnabled = false;
+			
+			ofstream logFile;
+			char buffer[128];
+			
+			sprintf(buffer, "robot-%f.log", Timer::GetFPGATimestamp());
+			logFile.open(buffer);
+			
 			CameraData::GetInstance().enabled = true;
 			cameraTask.Start();
+			
+			bool lightOn = false;
+			
+			string line;
+			ifstream targetFile;
+			char *a;
+			
+			bool wasAuto = false;
+			float currPoint = 90;
 			
 			while (IsOperatorControl() && IsEnabled()) {
 				GetWatchdog().Feed();
 				
-				turretPower = joystickManipulator.GetY();
+				cameraData.log = joystickManipulator.GetRawButton(5);
 				
-				if(turretPower > 0.2 || turretPower < -0.2)
-					motorTurret.Set(joystickManipulator.GetY() * TURRET_POWER);
-				else
-					motorTurret.Set(0);
+				if(joystickManipulator.GetRawButton(9)) {
+					lightOn = !lightOn;
+					if(lightOn)
+						relayLight.Set(Relay::kOn);
+					else
+						relayLight.Set(Relay::kOff);
+				}
 				
-				SmartDashboard::GetInstance()->PutDouble("centerX", CameraData::GetInstance().centerX);
-				SmartDashboard::GetInstance()->PutDouble("centerY", CameraData::GetInstance().centerY);
+				if(joystickManipulator.GetRawButton(8)) {
+					targetFile.open("target.txt");
+					if(targetFile.is_open()) {
+						while(targetFile.good()) {
+							getline(targetFile, line);
+							a = new char[line.size() + 1];
+							a[line.size()] = 0;
+							memcpy(a, line.c_str(), line.size());
+						}
+					}
+					currPoint = atof(a);
+					turretController->SetSetpoint(currPoint);
+					targetFile.close();
+				}
+				
+				if(joystickManipulator.GetRawButton(2)) {
+					wasAuto = true;
+					turretController->SetSetpoint(potentiometerMonitor->PIDGet() + cameraData.offsetAngleX);
+				} else if(wasAuto) {
+					wasAuto = false;
+					turretController->SetSetpoint(currPoint);
+				}
+				
+				if(joystickManipulator.GetRawButton(4)) {
+					gyroPosition = 0;
+					lastPosition = 0;
+					runTimer->Reset();
+					gyroTimer->Reset();
+					gyro.Reset();
+				}
+				
+				currentPosition = gyro.GetAngle();
+				gyroPosition += currentPosition - lastPosition;
+				lastPosition = currentPosition;
+				
+				//gyroPosition -= gyroTimer->Get() * 0.0238095238;
+				gyroTimer->Reset();
+				
+				SmartDashboard::GetInstance()->PutDouble("gyroPosition", gyroPosition);
+				sprintf(buffer, "[%f] %f\n", runTimer->Get(), gyroPosition);
+				logFile << buffer;
+				
+				if(joystickManipulator.GetRawButton(1)) {
+					if(!wasEnabled) {
+						turretController->Enable();
+						wasEnabled = true;
+					} else {
+						SmartDashboard::GetInstance()->PutDouble("lastOutput", turretOutput->lastOutput);
+					}
+				} else {
+					if(wasEnabled) {
+						turretController->Disable();
+						wasEnabled = false;
+					} else {
+						SmartDashboard::GetInstance()->PutDouble("lastOutput", 0);
+					}
+					
+					turretPower = joystickManipulator.GetX();
+					if(turretPower < -0.2 || turretPower > 0.2)
+						motorTurret.Set(joystickManipulator.GetX() * TURRET_POWER);
+					else
+						motorTurret.Set(0);
+				}
+				
+				SmartDashboard::GetInstance()->PutDouble("centerX", cameraData.centerX);
+				SmartDashboard::GetInstance()->PutDouble("centerY", cameraData.centerY);
+				SmartDashboard::GetInstance()->PutDouble("offsetAngleX", cameraData.offsetAngleX);
+				SmartDashboard::GetInstance()->PutDouble("offsetAngleY", cameraData.offsetAngleY);
 				SmartDashboard::GetInstance()->PutDouble("frames", CameraData::GetInstance().frames);
 				SmartDashboard::GetInstance()->PutDouble("fps", CameraData::GetInstance().fps);
 				
 				SmartDashboard::GetInstance()->PutDouble("analogPotentiometer", analogPotentiometer.GetVoltage());
-				SmartDashboard::GetInstance()->PutDouble("PotentiometerToDegrees", PotentiometerToDegrees(analogPotentiometer.GetVoltage()));
+				SmartDashboard::GetInstance()->PutDouble("PotentiometerToDegrees", potentiometerMonitor->PIDGet());
+				SmartDashboard::GetInstance()->PutDouble("setPoint", turretController->GetSetpoint());
 			}
+			
+			relayLight.Set(Relay::kOff);
+			
+			logFile.close();
+			
+			gyroTimer->Stop();
+			runTimer->Stop();
 			
 			CameraData::GetInstance().enabled = false;
 			cameraTask.Stop();
 			
 			GetWatchdog().SetEnabled(false);
+		}
+		
+		void RobotInit(void) {
+			printf("Hello, ninja h4X0r.");
 		}
 		
 		void Disabled (void) {
