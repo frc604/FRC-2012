c929f679ac8f7dd83c80d7d3b415732a2529b8d0
kevin <kevin.m.parker@gmail.com>
2012-02-12 19:53:42 -0800

    Fixed many of the errors in Orange.java (but not all errors)

--- src/com/_604robotics/robot2012/Robot2012Orange.java
+++ src/com/_604robotics/robot2012/Robot2012Orange.java
@@ -1,13 +1,20 @@
 package com._604robotics.robot2012;
 
-public class Robot2012Orange {
+import com.sun.squawk.util.MathUtils;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class Robot2012Orange extends SimpleRobot {
 
 	/* Actuator polarity and speed configuration. */
 	public static final double ACCELEROMETER_DRIVE_POWER	=0.5;
 	// TODO: Configure this.
 
-	public static final >> SOLENOID_SHIFTER_HIGH_POWER_DIRECTION DoubleSolenoid.kForward;
-	public static final >> SOLENOID_SHIFTER_LOW_POWER_DIRECTION DoubleSolenoid.kReverse;
+	public static final Value SOLENOID_SHIFTER_HIGH_POWER_DIRECTION =DoubleSolenoid.Value.kForward;
+	public static final Value SOLENOID_SHIFTER_LOW_POWER_DIRECTION  =DoubleSolenoid.Value.kReverse;
 
 	/* Sensor configuration. */
 	public static final double GYRO_DRIFT					=0.0238095238;
@@ -31,103 +38,106 @@ public class Robot2012Orange {
 	Compressor compressorPump;
 
 	Solenoid solenoidShifter;
-	
+
 	public Robot2012Orange() {
-			controller(XBOX_CONTROLLER_PORT),
-			driveTrain(new Victor(FRONT_LEFT_MOTOR_PORT), new Victor(REAR_LEFT_MOTOR_PORT), new Victor(FRONT_RIGHT_MOTOR_PORT), new Victor(REAR_RIGHT_MOTOR_PORT)),
-			gyroDriving(GYRO_PORT),
-			accelBalance(ACCELEROMETER_PORT),
-			solenoidShifter(SHIFTER_SOLENOID_PORT)
-			//{
-			GetWatchdog().SetEnabled(false);
-			driveTrain.SetSafetyEnabled(false);
+		controller = new Joystick(Ports.XBOX_CONTROLLER_PORT);
+		driveTrain = new RobotDrive(new Victor(Ports.FRONT_LEFT_MOTOR_PORT),
+				new Victor(Ports.REAR_LEFT_MOTOR_PORT),
+				new Victor(Ports.FRONT_RIGHT_MOTOR_PORT),
+				new Victor(Ports.REAR_RIGHT_MOTOR_PORT));
+		gyroDriving = new Gyro(Ports.GYRO_PORT);
+		accelBalance = new Accelerometer(Ports.ACCELEROMETER_PORT);
+		solenoidShifter = new Solenoid(Ports.SHIFTER_SOLENOID_PORT);
+		getWatchdog().setEnabled(false);
+		driveTrain.setSafetyEnabled(false);
 
-			driveTrain.SetInvertedMotor(RobotDrive.kFrontLeftMotor, true);
-			driveTrain.SetInvertedMotor(RobotDrive.kFrontRightMotor, true);
-			driveTrain.SetInvertedMotor(RobotDrive.kRearLeftMotor, true);
-			driveTrain.SetInvertedMotor(RobotDrive.kRearRightMotor, true);
+		driveTrain.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+		driveTrain.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
+		driveTrain.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+		driveTrain.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
 
-			accelBalance.SetSensitivity(ACCELEROMETER_SENSITIVITY);
+		accelBalance.setSensitivity(ACCELEROMETER_SENSITIVITY);
 
-			compressorPump = new Compressor(PRESSURE_SWITCH_PORT, COMPRESSOR_PORT);
+		compressorPump = new Compressor(Ports.PRESSURE_SWITCH_PORT, Ports.COMPRESSOR_PORT);
+	}
+
+	public static boolean isInRange(double xValue, double upperRange, double lowerRange) { // Self-explanatory.
+		return xValue <= upperRange && xValue >= lowerRange;
+	}
+
+	public static double deadband(double xValue, double upperBand, double lowerBand, double correctedValue) { // The antithesis of the BindToRange function
+		return (isInRange(xValue, upperBand, lowerBand))
+		? correctedValue 
+				: xValue;
+	}
+
+	public static double deadband(float xValue) { // The antithesis of the BindToRange function
+		return deadband(xValue, .1745, -.1745, 0.0);
+	}
+
+	public void Autonomous() {
+		getWatchdog().setEnabled(false);
+
+		compressorPump.start();
+
+		while(isAutonomous() && isEnabled()) {
+			// TODO: Write autonomous mode code
+		}
+
+		compressorPump.stop();
+	}
+
+	public void OperatorControl() {
+		getWatchdog().setEnabled(true);
+		driveTrain.setSafetyEnabled(true);
+
+		compressorPump.start();
+
+		float accelPower = 0;
+
+		// TODO: Move over gyro stuff from other project, once it's all hammered out.
+
+		while (isOperatorControl() && isEnabled()) {
+			getWatchdog().feed();
+
+			if (controller.getRawAxis(3) > 0.2 || controller.getRawAxis(3) < -0.2) { // XBOX: Left XOR Right trigger
+				solenoidShifter.set(SOLENOID_SHIFTER_HIGH_POWER_DIRECTION);
+				SmartDashboard.putString("Gear", "High");
+			} else {
+				solenoidShifter.set(SOLENOID_SHIFTER_LOW_POWER_DIRECTION);
+				SmartDashboard.putString("Gear", "Low");
 			}
 
-		public boolean IsInRange(float xValue, float upperRange, float lowerRange) { // Self-explanatory.
-			return xValue <= upperRange && xValue >= lowerRange;
-		}
-
-		public float Deadband(float xValue, float upperBand = 0.1745, float lowerBand = -0.1745, float correctedValue = 0.0) { // The antithesis of the BindToRange function
-			return (IsInRange(xValue, upperBand, lowerBand))
-			? correctedValue 
-					: xValue;
-		}
-
-		public void Autonomous() {
-			GetWatchdog().SetEnabled(false);
-
-			compressorPump.Start();
-
-			while(IsAutonomous() && IsEnabled()) {
-				// TODO: Write autonomous mode code
+			if (controller.getRawButton(Ports.ACCEL_BALANCE_BUTTON)) {
+				// TODO: Make this better.
+				accelPower = deadband(MathUtils.asin(accelBalance.getAcceleration())) / ACCELEROMETER_UPPER_RADIANS * ACCELEROMETER_DRIVE_POWER;
+				driveTrain.tankDrive(accelPower, accelPower);
+				SmartDashboard.putString("Drive Mode", "Balancing");
+				SmartDashboard.putDouble("Accel Output", accelPower);
+			} else {
+				driveTrain.tankDrive(controller.getRawAxis(2), controller.getRawAxis(5)); // Tank drive with left and right sticks on Xbox controller.
+				SmartDashboard.putString("Drive Mode", "Manual");
 			}
 
-			compressorPump.Stop();
-		}
-
-		public void OperatorControl() {
-			GetWatchdog().SetEnabled(true);
-			driveTrain.SetSafetyEnabled(true);
-
-			compressorPump.Start();
-
-			SmartDashboard dash = SmartDashboard.GetInstance();
-
-			float accelPower = 0;
-
-			// TODO: Move over gyro stuff from other project, once it's all hammered out.
-
-			while (IsOperatorControl() && IsEnabled()) {
-				GetWatchdog().Feed();
-
-				if (controller.GetRawAxis(3) > 0.2 || controller.GetRawAxis(3) < -0.2) { // XBOX: Left XOR Right trigger
-					solenoidShifter.Set(SOLENOID_SHIFTER_HIGH_POWER_DIRECTION);
-					dash.PutString("Gear", "High");
-				} else {
-					solenoidShifter.Set(SOLENOID_SHIFTER_LOW_POWER_DIRECTION);
-					dash.PutString("Gear", "Low");
-				}
-
-				if (controller.GetRawButton(ACCEL_BALANCE_BUTTON)) {
-					// TODO: Make this better.
-					accelPower = Deadband(asin(accelBalance.GetAcceleration())) / ACCELEROMETER_UPPER_RADIANS * ACCELEROMETER_DRIVE_POWER;
-					driveTrain.TankDrive(accelPower, accelPower);
-					dash.PutString("Drive Mode", "Balancing");
-					dash.PutDouble("Accel Output", accelPower);
-				} else {
-					driveTrain.TankDrive(controller.GetRawAxis(2), controller.GetRawAxis(5)); // Tank drive with left and right sticks on Xbox controller.
-					dash.PutString("Drive Mode", "Manual");
-				}
-
-				if (controller.GetRawButton(AIM_TURRET_BUTTON)) {
-					// TODO: Insert camera control, aiming components, when they're done, of course.
-				}
-
-				if (controller.GetRawButton(FIRE_BUTTON)) {
-					// TODO: Insert firing components, when they're done, of course.
-				}
+			if (controller.getRawButton(Ports.AIM_TURRET_BUTTON)) {
+				// TODO: Insert camera control, aiming components, when they're done, of course.
 			}
 
-			compressorPump.Stop();
-
-			driveTrain.SetSafetyEnabled(false);
-			GetWatchdog().SetEnabled(false);
+			if (controller.getRawButton(Ports.FIRE_BUTTON)) {
+				// TODO: Insert firing components, when they're done, of course.
+			}
 		}
 
-		public void Disabled() {
-			GetWatchdog().SetEnabled(false);
-			compressorPump.Stop();
-		}
+		compressorPump.stop();
+
+		driveTrain.setSafetyEnabled(false);
+		getWatchdog().setEnabled(false);
+	}
+
+	public void Disabled() {
+		getWatchdog().setEnabled(false);
+		compressorPump.stop();
+	}
 }
-}
 
-START_ROBOT_CLASS(Robot2012Orange);
+//START_ROBOT_CLASS(Robot2012Orange);
