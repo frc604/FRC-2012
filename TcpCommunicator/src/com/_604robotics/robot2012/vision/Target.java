63f6b727e58568df2232f4ad7b276de4f4442991
kevin <kevin.m.parker@gmail.com>
2012-02-21 13:57:38 -0800

    OK, should be done with UTF-8 issues...

--- TcpCommunicator/src/com/_604robotics/robot2012/vision/Target.java
+++ TcpCommunicator/src/com/_604robotics/robot2012/vision/Target.java
@@ -3,24 +3,57 @@ package com._604robotics.robot2012.vision;
 public class Target {
 	
 	/**
-	 * @param point
-	 * @param angle
+	 * This is the angle of the target, relative to the camera. </br> (angle) </br> .........(Target) </br> ......./
+	 * </br> ....../ </br> ...../ </br> ..../ - - - - - - - |> (Camera) </br> .../ </br> ../ </br> ./ </br>
+	 * 
+	 * </br>
+	 * 
+	 * this value is expressed in radians.
 	 */
-	public Target(Point3d point, double angle) {
-		// point p
-		this(point.x, point.y, point.z, angle);
-	}
+	public double	angle;
+	
 
+
+	/**
+	 * 
+	 * This is the uncertainty of the angle of the target.
+	 * 
+	 * This is interpreted as a plus or minus to the angle.
+	 * 
+	 * Again, this is expressed in radians
+	 * 
+	 * 
+	 */
+	public double	angle_uncertainty;
+	
+
+
+	/**
+	 * x, y, and z represent the 3-d position of the target
+	 * 
+	 * x will be positive when the target appears to be right of the center of the camera. y will be positive when the
+	 * target appears to be above of the center of the camera. z will always be negative (see <a
+	 * href="http://en.wikipedia.org/wiki/Right-hand_rule">Wikipedia: Right-hand rule</a>). As the absolute value of z
+	 * increases, so does the distance from the camera to the target.
+	 * 
+	 * To determine the approximate accuracy of these values, check [x, y, z]_accuracy.
+	 * 
+	 * The units of these measures are in inches.
+	 */
+	public double	x, y, z;
+	
+
+
+	/**
+	 * These are the uncertainties of the x, y, and z positions of the target.
+	 * 
+	 * These are interpreted as pluses and minuses to the x, y, and z values.
+	 * 
+	 * Again, these are in inches.
+	 */
+	public double	x_uncertainty, y_uncertainty, z_uncertainty;
 	
 	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	public String toString() {
-		return "Target [x=" + x + ", y=" + y + ", z=" + z + ", angle=" + angle + "]";
-	}
-
-
 
 	/**
 	 * @param x
@@ -35,9 +68,7 @@ public class Target {
 		this.z = z;
 		this.angle = angle;
 	}
-
-
-
+	
 	/**
 	 * @param x
 	 * @param y
@@ -59,61 +90,22 @@ public class Target {
 		this.angle = angle;
 		this.angle_uncertainty = angle_uncertainty;
 	}
-
-
-
-	/**
-	 * x, y, and z represent the 3-d position of the target
-	 * 
-	 * x will be positive when the target appears to be right of the center of the camera.
-	 * y will be positive when the target appears to be above of the center of the camera.
-	 * z will always be negative (see <a href="http://en.wikipedia.org/wiki/Right-hand_rule">Wikipedia: Right-hand rule</a>).
-	 * 		As the absolute value of z increases, so does the distance from the camera to the target.
-	 * 
-	 * To determine the approximate accuracy of these values, check [x, y, z]_accuracy.
-	 * 
-	 * The units of these measures are in inches.
-	 */
-	public double x, y, z;
+	
 	
 	/**
-	 * These are the uncertainties of the x, y, and z positions of the target.
-	 * 
-	 * These are interpreted as ±'s to the x, y, and z values.
-	 * 
-	 * Again, these are in inches.
+	 * @param point
+	 * @param angle
 	 */
-	public double x_uncertainty, y_uncertainty, z_uncertainty;
-
+	public Target(Point3d point, double angle) {
+		// point p
+		this(point.x, point.y, point.z, angle);
+	}
 	
-	/**
-	 * This is the angle of the target, relative to the camera.
-	 *												</br>
-	 * (angle)										</br>
-	 *.........(Target)								</br>
-	 *......./										</br>
-	 *....../										</br>
-	 *...../										</br>
-	 *..../ - - - - - - - |> (Camera)				</br>
-	 *.../											</br>
-	 *../											</br>
-	 *./											</br>
-	 * 
-	 * </br>
-	 * 
-	 * this value is expressed in radians.
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
 	 */
-	public double angle;
-
-	/**
-	 * 
-	 * This is the uncertainty of the angle of the target.
-	 * 
-	 * This is interpreted as a Plus or Minus to the angle.
-	 * 
-	 * Again, this is expressed in radians
-	 * 
-	 * 
-	 */
-	public double angle_uncertainty;
+	@Override
+	public String toString() {
+		return "Target [x=" + x + ", y=" + y + ", z=" + z + ", angle=" + angle + "]";
+	}
 }
