01a3dc401a55e8a2951f94fd00c68eac7db3bec3
Michael Smith <mdsmtp@gmail.com>
2012-02-21 19:30:34 -0800

    Cleaned branch.

--- Vision.h
+++
@@ -1,9 +0,0 @@
-/* 
- * TODO: Actually put something here.
- * 
- * See branch "vision-testing", where testing and construction
- * of the vision code are going on on the 2009 robot.
- * 
- * https://github.com/frc604/FRC-2012/tree/vision-testing
- * 
- */
