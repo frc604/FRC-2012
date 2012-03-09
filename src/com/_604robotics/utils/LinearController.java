b01746045b70873a9f1546c23d4aca39cb9ad95d
Michael Smith <mdsmtp@gmail.com>
2012-03-08 22:56:28 -0800

    Utils update.

--- src/com/_604robotics/utils/LinearController.java
+++ src/com/_604robotics/utils/LinearController.java
@@ -3,6 +3,20 @@ package com._604robotics.utils;
 import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.PIDSource;
 
+/**
+ * This class implements a controller with a horizontal segment, a linear
+ * segment, and finally a coasting segment.
+ * 
+ * When a target point is set, the controller decides which direction to go to
+ * get there, and then focuses on getting to that point or past it in that
+ * direction. If that condition is met, the output drops to zero. Else, if
+ * we're within a certain "coasting range", the output will be floored at the
+ * "coasting output". Else, if we're outside a certain "horizontal range", the
+ * output will be ceilinged at a certain "horizontal output". Else, the output
+ * will be scaled linearly between the two outputs.
+ * 
+ * @author  Michael Smith <mdsmtp@gmail.com>
+ */
 public class LinearController {
     private final PIDSource source;
     private final PIDOutput output;
@@ -16,11 +30,28 @@ public class LinearController {
     private double target = 0D;
     private boolean direction = false;
     
+    /**
+     * Internal enum to make remembering which direction is which easier.
+     */
     private interface Direction {
         public static final boolean FORWARD = true;
-        public static final boolean REVERSE = true;
+        public static final boolean REVERSE = false;
     }
     
+    /**
+     * Initializes a new LinearController.
+     * 
+     * @param   source              A PIDSource to read from.
+     * @param   output              A PIDOutput to write to.
+     * @param   horizontalRange     The horizontal range, as defined in the
+     *                              class description.
+     * @param   horizontalOutput    The horizontal output, as defined in the
+     *                              class description.
+     * @param   coastingRange       The coasting range, as defined in the class
+     *                              description.
+     * @param   coastingOutput      The coasting output, as defined in the class
+     *                              description.
+     */
     public LinearController (PIDSource source, PIDOutput output, double horizontalRange, double horizontalOutput, double coastingRange, double coastingOutput) {
         this.source = source;
         this.output = output;
@@ -30,20 +61,46 @@ public class LinearController {
         this.coastingOutput = coastingOutput;
     }
     
+    /**
+     * Updates the horizontal values.
+     * 
+     * @param   horizontalRange     The horizontal range, as defined in the
+     *                              class description.
+     * @param   horizontalOutput    The horizontal output, as defined in the
+     *                              class description.
+     */
     public void setHorizontalValues (double horizontalRange, double horizontalOutput) {
         this.horizontalRange = horizontalRange;
         this.horizontalOutput = horizontalOutput;
     }
     
+    /**
+     * Updates the coasting values.
+     * 
+     * @param   horizontalRange     The coasting range, as defined in the
+     *                              class description.
+     * @param   horizontalOutput    The coasting output, as defined in the
+     *                              class description.
+     */
     public void setCoastingRange (double coastingRange, double coastingOutput) {
         this.coastingRange = coastingRange;
         this.coastingOutput = coastingOutput;
     }
     
+    /**
+     * Gets the current target.
+     * 
+     * @return  The current target.
+     */
     public double getTarget () {
         return this.target;
     }
     
+    /**
+     * Sets the current target.
+     * 
+     * @param   target  The target to move toward.
+     */
     public void setTarget (double target) {
         this.target = target;
         if (this.target > this.source.pidGet())
@@ -52,6 +109,11 @@ public class LinearController {
             this.direction = Direction.REVERSE;
     }
     
+    /**
+     * Internal function that performs the output calculation.
+     * 
+     * @return  An output value, to be passed to a PIDOutput.
+     */
     private double calculate () {
         double pos = this.source.pidGet();
         double dist = Math.abs(pos - this.target);
@@ -62,12 +124,15 @@ public class LinearController {
         if (dist <= this.coastingRange)
             return this.coastingOutput;
         
-        if (dist <= this.horizontalRange)
+        if (dist >= this.horizontalRange)
             return this.horizontalOutput;
         
         return (this.horizontalOutput - this.coastingOutput) * ((dist - this.coastingRange) / (this.horizontalRange - this.coastingRange)) + this.coastingOutput;
     }
     
+    /**
+     * Updates the PIDOutput based on the latest data.
+     */
     public void update () {
         this.output.pidWrite(this.calculate());
     }
