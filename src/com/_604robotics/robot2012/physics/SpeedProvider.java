e2d54bf8d3ad667b01dedd590155260e9b02dd7a
kevin <kevin.m.parker@gmail.com>
2012-03-27 15:16:36 -0700

    added another method to SpeedProvider

--- src/com/_604robotics/robot2012/physics/SpeedProvider.java
+++ src/com/_604robotics/robot2012/physics/SpeedProvider.java
@@ -10,4 +10,17 @@ package com._604robotics.robot2012.physics;
  */
 public interface SpeedProvider {
     
+    /**
+     * Calculates the power to set the motor to
+     * @return the power for the motor
+     */
+    public double getMotorPower();
+    
+    /**
+     * Sets the target speed to the given value
+     * @param setSpeed  the value to set the target speed to
+     */
+    public void setSetSpeed(double setSpeed);
+    
+    public double getSetSpeed();
 }
