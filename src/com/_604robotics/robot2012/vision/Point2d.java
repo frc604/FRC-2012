da3431de4969772cc1a9c91b58d5adc474ddec9f
kevin <kevin.m.parker@gmail.com>
2012-03-29 14:21:30 -0700

    yeah, moved this file

---
+++ src/com/_604robotics/robot2012/vision/Point2d.java
@@ -0,0 +1,24 @@
+package com._604robotics.robot2012.vision;
+
+/**
+ * Represents a single point on the 2D plane.
+ * 
+ * @author  Kevin parker <kevin.m.parker@gmail.com>
+ */
+public class Point2d {
+	double x, y;
+
+	/**
+         * Intializes a new Point2d.
+         * 
+	 * @param   x   The x-coordinate of the point.
+	 * @param   y   The y-coordinate of the point.
+	 */
+	public Point2d(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+}
