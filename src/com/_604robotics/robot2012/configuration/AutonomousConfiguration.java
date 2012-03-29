a5c585a899e9d407b5d7b6e10e05fa9dc6e79ec3
Michael Smith <mdsmtp@gmail.com>
2012-03-29 00:15:07 -0700

    Autonomous restructuring; calibration in disabled mode.

--- src/com/_604robotics/robot2012/configuration/AutonomousConfiguration.java
+++ src/com/_604robotics/robot2012/configuration/AutonomousConfiguration.java
@@ -9,10 +9,8 @@ package com._604robotics.robot2012.configuration;
 public interface AutonomousConfiguration {
     public static final int MAX_STEP = 0;
     
-    public static final double STEP_1_FORWARD_TIME = 1D;
-    public static final double STEP_2_WAIT_TIME = 1D;
-    public static final double STEP_3_BACKWARD_TIME = 1D;
-    public static final double STEP_4_TURN_TIME = 1D;
-    public static final double STEP_5_FORWARD_TIME = 1D;
-    public static final double STEP_5_FORWARD_TIME_SIDES = 1D;
+    public static final double STEP_2_SHOOT_TIME = 1D;
+    public static final double STEP_3_TURN_TIME = 1D;
+    public static final double STEP_5_DRIVE_TIME = 1D;
+    public static final double STEP_6_WAIT_TIME = 1D;
 }
