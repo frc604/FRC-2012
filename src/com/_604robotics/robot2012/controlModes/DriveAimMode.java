f66ffe49996cbb580cd897933ee9caba1411a1e9
kevin <kevin.m.parker@gmail.com>
2012-04-20 16:01:08 -0700

    Added a DriveAim mode

---
+++ src/com/_604robotics/robot2012/controlModes/DriveAimMode.java
@@ -0,0 +1,22 @@
+package com._604robotics.robot2012.controlModes;
+
+/**
+ *
+ * @author Kevin Parker <kevin.m.parker@gmail.com>
+ */
+public class DriveAimMode extends ControlMode {
+
+    public boolean step() {
+        //TODO
+        return false;
+    }
+
+    public void init() {
+        //TODO
+    }
+
+    public void disable() {
+        //TODO
+    }
+
+}
