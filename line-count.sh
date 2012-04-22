b4bd1c879031f07e2c20897fa21e0a355bc33591
Michael Smith <mdsmtp@gmail.com>
2012-04-22 15:42:38 -0700

    Updated line-count.sh

--- line-count.sh
+++ line-count.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find ./src "*.java" | xargs wc -l | cut -c 1-8 | awk '{total += $1} END {print total}'
+find ./src -type f | xargs wc -l | cut -c 1-8 | awk '{total += $1} END {print total}'
